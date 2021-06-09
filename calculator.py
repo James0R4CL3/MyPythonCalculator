124cf5582f8891178468945586ea2703ec5ce6c0
James Buckman <jamesrbuckman@gmail.comgit add index.htmlgit status$ git statuslearclear>
2021-06-09 17:11:14 +1000

    complete

--- calculator.py
+++ calculator.py
@@ -1,5 +1,34 @@
+#My Python Calculator (Kinda)
 
+operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+** for exponent
+''')
 
+number_1 = int(input('Enter your first number: '))
+number_2 = int(input('Enter your second number: '))
 
+if operation == '+':
+    print('{} + {} = '.format(number_1, number_2))
+    print(number_1 + number_2)
 
+elif operation == '-':
+    print('{} - {} = '.format(number_1, number_2))
+    print(number_1 - number_2)
 
+elif operation == '*':
+    print('{} * {} = '.format(number_1, number_2))
+    print(number_1 * number_2)
+
+elif operation == '/':
+    print('{} / {} = '.format(number_1, number_2))
+    print(number_1 / number_2)
+
+elif operation == '**':
+    print(number_1 ** number_2)
+else:
+    print('You have not typed a valid operator, please run the program again.')
