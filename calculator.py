0647e7ca7eee5e7f4b539f3e961762d1ff9707c7
James Buckman <jamesrbuckman@gmail.comgit add index.htmlgit status$ git statuslearclear>
2021-06-09 18:14:06 +1000

    added feature

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 #My Python Calculator (Kinda)
 
-def Primary_operation():
+def main():
     operation = input('''
     Please type in the math operation you would like to complete:
     + for addition
@@ -10,40 +10,46 @@ def Primary_operation():
     ** for exponent
     ''')
 
-Primary_operation()
+
+    number_1 = int(input('Enter your first number: '))
+    number_2 = int(input('Enter your second number: '))
+
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+        input('Press "Enter" for new operation :) ')
+        main()
 
 
-number_1 = int(input('Enter your first number: '))
-number_2 = int(input('Enter your second number: '))
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+        input('Press "Enter" for new operation :) ')
+        input('Press "Enter" for new operation :) ')
+        main()
 
-if operation == '+':
-    print('{} + {} = '.format(number_1, number_2))
-    print(number_1 + number_2)
-    input('Press "Enter" for new operation :) ')
-    Primary_operation()
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+        input('Press "Enter" for new operation :) ')
+        input('Press "Enter" for new operation :) ')
+        main()
 
-elif operation == '-':
-    print('{} - {} = '.format(number_1, number_2))
-    print(number_1 - number_2)
-    input('Press "Enter" for new operation :) ')
-    Primary_operation()
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+        input('Press "Enter" for new operation :) ')
+        input('Press "Enter" for new operation :) ')
+        main()
 
-elif operation == '*':
-    print('{} * {} = '.format(number_1, number_2))
-    print(number_1 * number_2)
-    input('Press "Enter" for new operation :) ')
-    Primary_operation()
+    elif operation == '**':
+        print(number_1 ** number_2)
+        input('Press "Enter" for new operation :) ')
+        input('Press "Enter" for new operation :) ')
+        main()
 
-elif operation == '/':
-    print('{} / {} = '.format(number_1, number_2))
-    print(number_1 / number_2)
-    input('Press "Enter" for new operation :) ')
-    Primary_operation()
-
-elif operation == '**':
-    print(number_1 ** number_2)
-    input('Press "Enter" for new operation :) ')
-    Primary_operation()
-
-else:
-    print('You have not typed a valid operator, please run the program again.')
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+        input('Press "Enter" for new operation :) ')
+        main()
+main()
